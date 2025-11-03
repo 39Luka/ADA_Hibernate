a00b069090ca7e811cce35c85408a36a8e236ad9
silcaclei <silviacachonleiva@gmail.com>
2025-11-03 16:56:59 +0100

    v2

--- src/main/java/org/example/dao/GenericDao.java
+++ src/main/java/org/example/dao/GenericDao.java
@@ -1,5 +1,13 @@
 package org.example.dao;
 
+
+import org.hibernate.Session;
+
 public interface GenericDao<T, ID> {
 
+    ID save(Session session, T entity);
+    T findById(Session session, ID id);
+    T update(Session session, T entity);
+    void delete(Session session, T entity);
+    boolean deleteById(Session session, ID id);
 }
